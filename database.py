b93f651e279df74f55cc72556b8d936cc8f9c91f
KhorSL <stephenksl@gmail.com>
2019-06-30 22:57:59 +0800

    Add inline date selection

--- database.py
+++ database.py
@@ -7,22 +7,24 @@ class Database:
         self.dbname = dbname
 
         """Development"""
-        self.conn = psycopg2.connect(user = user, password = password, host = host, port = port, database = dbname)
+        # self.conn = psycopg2.connect(user = user, password = password, host = host, port = port, database = dbname)
         
         """Deployment"""
-        # DATABASE_URL = os.environ['DATABASE_URL']
-        # self.conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        DATABASE_URL = os.environ['DATABASE_URL']
+        self.conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
         self.cursor = self.conn.cursor()
 
     def setup(self):
-        stmt = "CREATE TABLE IF NOT EXISTS reminders (id BIGSERIAL PRIMARY KEY, chat_id BIGSERIAL NOT NULL, reminder_text TEXT, reminder_time TIMESTAMP)"
-        self.cursor.execute(stmt)
+        table_reminders = "CREATE TABLE IF NOT EXISTS reminders (id BIGSERIAL PRIMARY KEY, chat_id BIGSERIAL NOT NULL, reminder_text TEXT, reminder_time TIMESTAMP WITH TIME ZONE)"
+        table_intermediate = "CREATE TABLE IF NOT EXISTS intermediate (id BIGSERIAL PRIMARY KEY, chat_id BIGSERIAL NOT NULL, reminder_text TEXT, stored_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP)"
+        self.cursor.execute(table_reminders)
+        self.cursor.execute(table_intermediate)
         self.conn.commit()
 
-    def add_reminder(self, input_text, chat_id):
-        stmt = "INSERT INTO reminders (reminder_text, chat_id) VALUES (%s, %s)"
-        args = (input_text, chat_id, )
+    def add_reminder(self, input_text, chat_id, reminder_time):
+        stmt = "INSERT INTO reminders (reminder_text, chat_id, reminder_time) VALUES (%s, %s, %s)"
+        args = (input_text, chat_id, reminder_time, )
         self.cursor.execute(stmt, args)
         self.conn.commit()
 
@@ -37,3 +39,21 @@ class Database:
         args = (chat_id, )
         self.cursor.execute(stmt, args)
         return [x[0] for x in self.cursor.fetchall()]
+
+    def add_intermediate(self, input_text, chat_id):
+        stmt = "INSERT INTO intermediate (reminder_text, chat_id) VALUES (%s, %s)"
+        args = (input_text, chat_id, )
+        self.cursor.execute(stmt, args)
+        self.conn.commit()
+
+    def get_intermediate(self, chat_id):
+        stmt = "SELECT reminder_text FROM intermediate WHERE chat_id = (%s) ORDER BY stored_time ASC LIMIT 1"
+        args = (chat_id, )
+        self.cursor.execute(stmt, args)
+        return [x[0] for x in self.cursor.fetchall()]
+
+    def delete_intermediate(self, chat_id):
+        stmt = "DELETE FROM intermediate WHERE id = (SELECT id FROM intermediate WHERE chat_id = (%s) ORDER BY stored_time ASC LIMIT 1);"
+        args = (chat_id, )
+        self.cursor.execute(stmt, args)
+        self.conn.commit()
--- main.py
+++ main.py
@@ -5,19 +5,56 @@ from telegram.ext import Updater, InlineQueryHandler, CommandHandler, MessageHan
 
 from database import Database
 
+import telegramcalendar
+from telegram import ReplyKeyboardRemove
+
+import re
+
 db = Database()
 
 token = '895938816:AAEe0YkgUOjoOmQjvYEiErYBFxA9qhcChMY'
 PORT = int(os.environ.get('PORT', '8443'))
 
 # Helper methods
+def numbering_list(input_list):
+    result = ""
+    for i in range(len(input_list)):
+        result = result + str(i+1) + ". " + input_list[i] + "\n"
+    return result
+
 def reply_user(update, reminders):
     if len(reminders) > 0:
-        message = "\n".join(reminders)
+        # message = "\n".join(reminders)
+        message = numbering_list(reminders)
         update.message.reply_text(message)
     else:
         message = "There are no reminders in your list."
         update.message.reply_text(message)
+
+def inline_handler(bot, update):
+    query = update.callback_query
+
+    if re.match(r"DAY;\d+;\d+;\d+", query.data):
+        selected, date = telegramcalendar.process_calendar_selection(bot, update)
+
+        if selected:
+            chat_id = query.message.chat_id
+            bot.send_message(text="You selected %s" % (date.strftime("%d/%m/%Y")), chat_id=chat_id, reply_markup=ReplyKeyboardRemove())
+            
+            # Retrieve and remove user input from intermediate table
+            input_reminder = db.get_intermediate(chat_id)[0]
+            db.delete_intermediate(chat_id)
+
+            db.add_reminder(input_reminder, chat_id, date)
+            reminders = db.get_reminders(chat_id)
+            
+            if len(reminders) > 0:
+                message = numbering_list(reminders)
+                bot.send_message(text=message, chat_id=chat_id)
+            else:
+                message = "There are no reminders in your list."
+                bot.send_message(text=message, chat_id=chat_id)
+
 
 def start(bot, update):
     chat_id = update.message.chat_id
@@ -28,14 +65,19 @@ def help(bot, update):
 
 def remind(bot, update):
     chat_id = update.message.chat_id
-    input = update.message.text[8:]
+    input_reminder = update.message.text[8:]
+
+    # Delete any leftover intermediate result for current chat
+    db.delete_intermediate(chat_id)
+
+    # Save intermediate result
+    db.add_intermediate(input_reminder, chat_id)
+
     reminders = db.get_reminders(chat_id)
     if input in reminders:
         pass
     else:
-        db.add_reminder(input, chat_id)
-        reminders = db.get_reminders(chat_id)
-    reply_user(update, reminders)
+        update.message.reply_text("Please select a date: ", reply_markup=telegramcalendar.create_calendar())
 
 def delete(bot, update):
     chat_id = update.message.chat_id
@@ -60,44 +102,46 @@ def error(bot, update, error):
 
 def main():
     """Deployment"""
-    # db.setup()
-    # updater = Updater(token)
-    # updater.start_webhook(listen="0.0.0.0",
-    #                   port=PORT,
-    #                   url_path=token)
-    
-    # dp = updater.dispatcher
-
-    # dp.add_handler(CommandHandler("start", start))
-    # dp.add_handler(CommandHandler("help", help))
-    # dp.add_handler(CommandHandler("remind", remind))
-    # dp.add_handler(CommandHandler("delete", delete))
-    # dp.add_handler(CommandHandler("list", list_all))
-	
-    # updater.bot.set_webhook("https://remindeer-bot.herokuapp.com/" + token)
-    # updater.idle()
-
-    """Development"""
     db.setup()
-
     updater = Updater(token)
-
+    updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=token)
+    
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("remind", remind))
     dp.add_handler(CommandHandler("delete", delete))
     dp.add_handler(CommandHandler("list", list_all))
-
-    # log all errors
-    dp.add_error_handler(error)
-
-    # Start the Bot
-    updater.start_polling()
-
+    dp.add_handler(CallbackQueryHandler(inline_handler))
+	
+    updater.bot.set_webhook("https://remindeer-bot.herokuapp.com/" + token)
     updater.idle()
+
+    """Development"""
+    # db.setup()
+
+    # updater = Updater(token)
+
+    # dp = updater.dispatcher
+
+    # # on different commands - answer in Telegram
+    # dp.add_handler(CommandHandler("start", start))
+    # dp.add_handler(CommandHandler("help", help))
+    # dp.add_handler(CommandHandler("remind", remind))
+    # dp.add_handler(CommandHandler("delete", delete))
+    # dp.add_handler(CommandHandler("list", list_all))
+    # dp.add_handler(CallbackQueryHandler(inline_handler))
+
+    # # log all errors
+    # dp.add_error_handler(error)
+
+    # # Start the Bot
+    # updater.start_polling()
+
+    # updater.idle()
 
 if __name__ == '__main__':
     print ('Bot is running')
