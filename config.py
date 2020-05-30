504cbc2f7ad83ee5e7b9430a1f8ad951e26cac83
KhorSL <stephenksl@gmail.com>
2020-05-30 15:28:40 +0800

    config.py: fix bug, convert string to int

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ TOKEN = os.environ['BOT_TOKEN']
 PORT = int(os.environ.get('PORT', '8443'))
 APP_URL = os.environ['APP_URL']
 DATABASE_URL = os.environ['DATABASE_URL']
-API_KEY_LEN = os.environ['API_KEY_LEN']
-SALT_LEN = os.environ['SALT_LEN']
+API_KEY_LEN = int(os.environ['API_KEY_LEN'])
+SALT_LEN = int(os.environ['SALT_LEN'])
 HASH_ALGO = os.environ['HASH_ALGO']
-HASH_ITER = os.environ['HASH_ITER']
+HASH_ITER = int(os.environ['HASH_ITER'])
 
 # Default Timezone
 DEFAULT_TZ = timezone('Asia/Singapore')
